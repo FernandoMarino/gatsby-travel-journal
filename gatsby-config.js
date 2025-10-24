2da92085f4d65c3be8fed88fd99dff9d455588d5
FernandoMarino <fernandomarinosantos@gmail.com>
2025-10-23 21:29:56 -0700

    Initial Commit - Setting Up Project

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,9 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+	siteMetadata: {
+		siteUrl: `https://www.yourdomain.tld`,
+		title: `Home Keeper`,
+	},
+	plugins: [],
+};
